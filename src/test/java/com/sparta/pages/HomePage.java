9e13623a4010a42e8b51d869c8735c9d0b1ad4d5
Aiden Farthing <aiden.farthing@hotmail.co.uk>
2025-11-13 11:23:10 +0000

    Home page added

--- src/test/java/com/sparta/pages/HomePage.java
+++ src/test/java/com/sparta/pages/HomePage.java
@@ -1,4 +1,71 @@
 package com.sparta.pages;
 
-public class HomePage {
+import net.serenitybdd.core.annotations.findby.FindBy;
+import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.core.pages.WebElementFacade;
+
+public class HomePage extends PageObject {
+
+    // Navigation Link
+    @FindBy(css = "a[href='/']")
+    private WebElementFacade homeLink;
+
+    @FindBy(css = "a[href='/products']")
+    private WebElementFacade InventoryLink;
+
+    @FindBy(css = "a[href='/view_cart']")
+    private WebElementFacade cartLink;
+
+    @FindBy(css = "a[href='/login']")
+    private WebElementFacade loginLink;
+
+    @FindBy(css = "a[href='/contact_us']")
+    private WebElementFacade contactUsLink;
+
+    @FindBy(css = "a[href='/product_details/1']")
+    private WebElementFacade productLink;
+    // Main
+    @FindBy(css = "#slider .carousel-inner")
+    private WebElementFacade slider;
+
+    @FindBy(css = ".features_items")
+    private WebElementFacade featuredItemsSection;
+
+
+    /** Navigate to the Products page */
+    public void viewProduct() {
+        productLink.click();
+    }
+
+    public void openInventory() {
+        InventoryLink.click();
+    }
+
+    public void openCart() {
+        cartLink.click();
+    }
+
+    public void openLogin() {
+        loginLink.click();
+    }
+
+    public void openContactUs() {
+        contactUsLink.click();
+    }
+
+    public boolean isHomePageLoaded() {
+        return slider.isVisible() && featuredItemsSection.isVisible();
+    }
+
+    public boolean isNavigationVisible() {
+        return homeLink.isVisible() &&
+                productLink.isVisible() &&
+                cartLink.isVisible() &&
+                loginLink.isVisible() &&
+                contactUsLink.isVisible();
+    }
+
+    public boolean isFeatureSectionVisible() {
+        return featuredItemsSection.isVisible();
+    }
 }
