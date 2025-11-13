4db2258ed022ad5f49f55b20dd9f65aabda6ef3b
Ahmed Hamad <a.g.hamad@icloud.com>
2025-11-13 11:55:08 +0000

    Added the filter feature and stepDefs

---
+++ src/test/java/com/sparta/steps/FilterStepdefs.java
@@ -0,0 +1,19 @@
+package com.sparta.steps;
+
+import io.cucumber.java.PendingException;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class FilterStepdefs {
+    @When("I can filter them by category \\(e.g.Women)")
+    public void iCanFilterThemByCategoryEGWomen() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("I can easily find the type of items I'm interested in.")
+    public void iCanEasilyFindTheTypeOfItemsIMInterestedIn() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
