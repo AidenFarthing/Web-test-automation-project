c03a0c65229a0a41d0217bd2db6e3e0922cb6138
Lucy Hudson <lucyfetherhudson@gmail.com>
2025-11-13 09:47:55 +0000

    pom file

--- src/test/java/sparta/pages/CartPage.java
+++ src/test/java/sparta/pages/CartPage.java
@@ -1,4 +1,10 @@
 package sparta.pages;
 
-public class CartPage {
+import net.serenitybdd.core.pages.WebElementFacade;
+import net.thucydides.core.annotations.DefaultUrl;
+import net.thucydides.core.pages.PageObject;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.support.FindBy;
+
+public class CartPage  extends PageObject {
 }
