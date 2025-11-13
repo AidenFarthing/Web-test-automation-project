791313319aa0a09ed6287dd0b5b2ff03cf4bed5f
nahisah <nahisahnasleem@gmail.com>
2025-11-13 14:51:29 +0000

    Login Tests

---
+++ src/test/java/com/sparta/runners/TestRunner.java
@@ -0,0 +1,14 @@
+package com.sparta.runners;
+
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "com.sparta.steps",
+        plugin = {"pretty"}
+)
+public class TestRunner {}
