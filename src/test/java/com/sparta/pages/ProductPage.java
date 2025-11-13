e541ea1461d7cdf58801adcb446411ecf9edd14a
Lucy Hudson <lucyfetherhudson@gmail.com>
2025-11-13 10:40:17 +0000

    productPage

--- src/test/java/com/sparta/pages/ProductPage.java
+++ src/test/java/com/sparta/pages/ProductPage.java
@@ -1,4 +1,16 @@
 package com.sparta.pages;
 
-public class ProductPage {
+import net.serenitybdd.core.pages.WebElementFacade;
+import net.thucydides.core.pages.PageObject;
+import org.openqa.selenium.support.FindBy;
+
+public class ProductPage  extends PageObject {
+
+    @FindBy(className = "btn")
+    private WebElementFacade addToCartButton;
+
+    public void clickAddToCartButton() {
+        addToCartButton.click();
+    }
+
 }
--- src/test/java/com/sparta/steps/CartStepdefs.java
+++ src/test/java/com/sparta/steps/CartStepdefs.java
@@ -1,20 +1,27 @@
+package com.sparta.steps;
+
+import com.sparta.pages.HomePage;
 import io.cucumber.java.PendingException;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.pages.PageObject;
 
-public class CartStepdefs {
+public class CartStepdefs  extends PageObject {
+    @Managed
+    HomePage homePage;
+
     @Given("I am on the products page")
     public void iAmOnTheProductsPage() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        homePage.open();
+        homePage.viewProduct();
     }
 
     @When("I add an item to cart")
     public void iAddAnItemToCart() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+
     }
 
     @Then("there should an item in my cart")
