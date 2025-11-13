1710bf13b9d2fb3f782f2475d75c1b686bbff350
Ahmed Hamad <a.g.hamad@icloud.com>
2025-11-13 14:32:52 +0000

    Still trying to get the filter tests to run

--- src/test/java/com/sparta/steps/FilterStepdefs.java
+++ src/test/java/com/sparta/steps/FilterStepdefs.java
@@ -1,4 +1,31 @@
 package com.sparta.steps;
 
+
+import com.sparta.pages.InventoryPage;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import net.thucydides.core.annotations.Managed;
+import org.junit.jupiter.api.Assertions;
+
 public class FilterStepdefs {
+    @Managed
+    InventoryPage inventoryPage;
+
+    @Given("I am on the inventory page")
+    public void iAmOnTheInventoryPage() {
+        inventoryPage.open();
+
+    }
+
+    @When("I can filter them by category \\(e.g.Women)")
+    public void iCanFilterThemByCategoryEGWomen() {
+        inventoryPage.setWomenFliter();
+        inventoryPage.setDressFliter();
+    }
+
+    @Then("I can easily find the type of items I'm interested in.")
+    public void iCanEasilyFindTheTypeOfItemsIMInterestedIn() {
+        Assertions.assertEquals(3,inventoryPage.getInventory());
+    }
 }
